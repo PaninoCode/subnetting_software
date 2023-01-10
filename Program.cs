9b0c77152c8f3bc5871934c7b1be03e0f6b2fa47
alex65ferrari <41290578+PaninoCode@users.noreply.github.com>
2023-01-10 12:22:29 +0100

    code cleanup, --help flag

--- Program.cs
+++ Program.cs
@@ -1,82 +1,104 @@
-﻿namespace networkTools {
-    class Program {
-        static void Main(string[] args) {
-            
-            if(args.Length < 2) {
-                Console.WriteLine("Command usage: networkTools <ip address> <subnet mask> (flags)");
+﻿namespace networkTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Any(arg => arg == "--help"))
+            {
+                Console.WriteLine("List of usable flags: \n--ipAddress              Prints the Ip Address\n--subnetMask             Prints the Subnet Mask\n--ipBits                 Prints the Ip Address in Binary\n--smBits                 Prints the Subnet Mask in Binary\n--networkAddress         Prints the Network Address\n--broadcastAddress       Prints the Broadcast Address\n--wildcardMask           Prints the Wildcard Mask\n--numberOfHosts          Prints the Number of Hosts\n--numberOfUsableHosts    Prints the Number of Usable Hosts\n--firstUsableHost        Prints the First Usable Host's Ip Address\n--lastUsableHost         Prints the Last Usable Host's Ip Address\n--hostsRange             Prints the Range of Usable Hosts\n--addressClass           Prints the Class of the Ip Address\n--all                    Prints everything");
+                return;
+            }
+            if (args.Length <= 2)
+            {
+                Console.WriteLine("Command usage: networkTools <ip address> <subnet mask> (flags) \nUse --help to display a list of flags");
                 return;
             }
 
             Ip ip = new Ip();
 
-            if(!ip.SetIpAddress(args[0].ToString())){
+            if (!ip.SetIpAddress(args[0].ToString()))
+            {
                 Console.WriteLine(args[0] + " is not a valid ip address");
                 return;
             }
 
-            if(!ip.SetSubnetMask(args[1].ToString())){
+            if (!ip.SetSubnetMask(args[1].ToString()))
+            {
                 Console.WriteLine(args[1] + " is not a valid subnet mask");
                 return;
             }
 
-            if(args.Length > 2){
-                if(args.Any(arg => arg == "--ipAddress")){
-                    Console.WriteLine("Ip Address: " + ip.GetIpAddress());
-                }
-                if(args.Any(arg => arg == "--subnetMask")){
-                    Console.WriteLine("Mask: " + ip.GetSubnetMask());
-                }
-                if(args.Any(arg => arg == "--ipBits")){
-                    Console.WriteLine("Ip Bits: " + ip.GetIpBits());
-                }
-                if(args.Any(arg => arg == "--smBits")){
-                    Console.WriteLine("Mask Bits: " + ip.GetSmBits());
-                }
-                if(args.Any(arg => arg == "--networkAddress")){
-                    Console.WriteLine("Network Address: " + ip.GetNetworkAddress());
-                }
-                if(args.Any(arg => arg == "--broadcastAddress")){
-                    Console.WriteLine("Broadcast Address: " + ip.GetBroadcastAddress());
-                }
-                if(args.Any(arg => arg == "--wildcardMask")){
-                    Console.WriteLine("Wildcard Mask: " + ip.GetWildcardMask());
-                }
-                if(args.Any(arg => arg == "--numberOfHosts")){
-                    Console.WriteLine("Number of Hosts: " + ip.GetNumberOfHosts());
-                }
-                if(args.Any(arg => arg == "--numberOfUsableHosts")){
-                    Console.WriteLine("Number of Usable Hosts: " + ip.GetNumberOfUsableHosts());
-                }
-                if(args.Any(arg => arg == "--firstUsableHost")){
-                    Console.WriteLine("First Usable Host: " + ip.GetFirstUsableHost());
-                }
-                if(args.Any(arg => arg == "--lastUsableHost")){
-                    Console.WriteLine("Last Usable Host: " + ip.GetLastUsableHost());
-                }
-                if(args.Any(arg => arg == "--hostsRange")){
-                    Console.WriteLine("Hosts Range: " + ip.GetHostsRange());
-                }
-                if(args.Any(arg => arg == "--addressClass")){
-                    Console.WriteLine("Address Class: " + ip.GetAddressClass());
-                }
-                if(args.Any(arg => arg == "--all")){
-                    Console.WriteLine("Ip Address: " + ip.GetIpAddress());
-                    Console.WriteLine("Mask: " + ip.GetSubnetMask());
-                    Console.WriteLine("Ip Bits: " + ip.GetIpBits());
-                    Console.WriteLine("Mask Bits: " + ip.GetSmBits());
-                    Console.WriteLine("Network Address: " + ip.GetNetworkAddress());
-                    Console.WriteLine("Broadcast Address: " + ip.GetBroadcastAddress());
-                    Console.WriteLine("Wildcard Mask: " + ip.GetWildcardMask());
-                    Console.WriteLine("Number of Hosts: " + ip.GetNumberOfHosts());
-                    Console.WriteLine("Number of Usable Hosts: " + ip.GetNumberOfUsableHosts());
-                    Console.WriteLine("First Usable Host: " + ip.GetFirstUsableHost());
-                    Console.WriteLine("Last Usable Host: " + ip.GetLastUsableHost());
-                    Console.WriteLine("Hosts Range: " + ip.GetHostsRange());
-                    Console.WriteLine("Address Class: " + ip.GetAddressClass());
-                }
-            }else{
-                Console.WriteLine("No flags specified. Check the readme file for a list of usable flags.");
+
+            string[] allFlags = { "--ipAddress", "--subnetMask", "--ipBits", "--smBits", "--networkAddress", "--broadcastAddress", "--wildcardMask", "--numberOfHosts", "--numberOfUsableHosts", "--firstUsableHost", "--lastUsableHost", "--hostsRange", "--addressClass" };
+            string[] flags = new string[args.Length - 2];
+
+            if (args.Any(arg => arg == "--all"))
+            {
+                flags = allFlags;
             }
+            else
+            {
+                for (int i = 0; i < args.Length - 2; i++)
+                {
+                    if (allFlags.Any(flag => flag == args[i + 2]))
+                    {
+                        flags[i] = args[i + 2];
+                    }
+                    else
+                    {
+                        Console.WriteLine(args[i + 2] + " is not a valid flag");
+                        return;
+                    }
+                }
+            }
+
+            for (int i = 0; i < flags.Length; i++)
+            {
+                switch (flags[i])
+                {
+                    case "--ipAddress":
+                        Console.WriteLine("Ip Address: " + ip.GetIpAddress());
+                        break;
+                    case "--subnetMask":
+                        Console.WriteLine("Mask: " + ip.GetSubnetMask());
+                        break;
+                    case "--ipBits":
+                        Console.WriteLine("Ip Bits: " + ip.GetIpBits());
+                        break;
+                    case "--smBits":
+                        Console.WriteLine("Mask Bits: " + ip.GetSmBits());
+                        break;
+                    case "--networkAddress":
+                        Console.WriteLine("Network Address: " + ip.GetNetworkAddress());
+                        break;
+                    case "--broadcastAddress":
+                        Console.WriteLine("Broadcast Address: " + ip.GetBroadcastAddress());
+                        break;
+                    case "--wildcardMask":
+                        Console.WriteLine("Wildcard Mask: " + ip.GetWildcardMask());
+                        break;
+                    case "--numberOfHosts":
+                        Console.WriteLine("Number of Hosts: " + ip.GetNumberOfHosts());
+                        break;
+                    case "--numberOfUsableHosts":
+                        Console.WriteLine("Number of Usable Hosts: " + ip.GetNumberOfUsableHosts());
+                        break;
+                    case "--firstUsableHost":
+                        Console.WriteLine("First Usable Host: " + ip.GetFirstUsableHost());
+                        break;
+                    case "--lastUsableHost":
+                        Console.WriteLine("Last Usable Host: " + ip.GetLastUsableHost());
+                        break;
+                    case "--hostsRange":
+                        Console.WriteLine("Hosts Range: " + ip.GetHostsRange());
+                        break;
+                    case "--addressClass":
+                        Console.WriteLine("Address Class: " + ip.GetAddressClass());
+                        break;
+                }
+            }
+
         }
     }
 }
